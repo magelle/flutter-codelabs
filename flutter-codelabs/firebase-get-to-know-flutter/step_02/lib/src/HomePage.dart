62ea8af846c9ec823cf07880312d2dade65cc087
Maxime GELLE <maxime.gelle@gmail.com>
2020-10-25 16:47:17 +0100

    Google codelabs - add Get to know Firebase for Flutter - Write and Read message from Firestore

---
+++ flutter-codelabs/firebase-get-to-know-flutter/step_02/lib/src/HomePage.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:gtk_flutter/src/authentication.dart';
+import 'package:gtk_flutter/src/applicationState.dart';
+import 'package:gtk_flutter/src/guestBook.dart';
+import 'package:gtk_flutter/src/widgets.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase Meetup'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Image.asset('assets/codelab.png'),
+          SizedBox(height: 8),
+          IconAndDetail(Icons.calendar_today, 'October 30'),
+          IconAndDetail(Icons.location_city, 'San Francisco'),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Authentication(
+              email: appState.email,
+              loginState: appState.loginState,
+              startLoginFlow: appState.startLoginFlow,
+              verifyEmail: appState.verifyEmail,
+              signInWithEmailAndPassword: appState.signInWithEmailAndPassword,
+              cancelRegistration: appState.cancelRegistration,
+              registerAccount: appState.registerAccount,
+              signOut: appState.signOut,
+            ),
+          ),
+          Divider(
+            height: 8,
+            thickness: 1,
+            indent: 8,
+            endIndent: 8,
+            color: Colors.grey,
+          ),
+          Header("What we'll be doing"),
+          Paragraph(
+            'Join us for a day full of Firebase Workshops and Pizza!',
+          ),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loginState == ApplicationLoginState.loggedIn) ...[
+                  Header('Discussion'),
+                  GuestBook(
+                    addMessage: (String message) =>
+                        appState.addMessageToGuestBook(message),
+                    messages: appState.guestBookMessages,
+                  ),
+                ]
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
--- install_flutter.sh
+++ install_flutter.sh
@@ -56,11 +56,19 @@ flutter config --enable-web
 
 flutter run -d web
 
-Resources :
-https://flutter.dev/docs/cookbook
-https://flutter.dev/docs/resources/bootstrap-into-dart
-https://dart.dev/codelabs
-https://pub.dev/
+# Resources :
+# https://flutter.dev/docs/cookbook
+# https://flutter.dev/docs/resources/bootstrap-into-dart
+# https://dart.dev/codelabs
+# https://pub.dev/
 
+# Firestore Doc
+https://console.firebase.google.com/project/fir-flutter-codelab-cf878/authentication/users
+
+# Flutter youtub playlist
+https://www.youtube.com/playlist?list=PLOU2XLYxmsIJ7dsVN4iRuA7BT8XHzGtCr
+
+# Firestore youtube playlist
+https://www.youtube.com/playlist?list=PLl-K7zZEsYLluG5MCVEzXAQ7ACZBCuZgZ
 
 
