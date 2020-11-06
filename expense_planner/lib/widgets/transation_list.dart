3b6d19038e6ccae7957987be61214eb2ec9962eb
Maxime GELLE <maxime.gelle@gmail.com>
2020-11-07 00:18:23 +0100

    Remove transaction

--- expense_planner/lib/widgets/transation_list.dart
+++ expense_planner/lib/widgets/transation_list.dart
@@ -15,61 +15,53 @@ class TransactionList extends StatelessWidget {
       height: 300,
       child: userTransactions.isEmpty
           ? Column(
-        children: [
-          Text(
-            'No transactions added yet',
-            style: Theme
-                .of(context)
-                .textTheme
-                .title,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            height: 200,
-            child: Image.asset(
-              'assets/images/sleeping.png',
-              fit: BoxFit.cover,
-            ),
-          )
-        ],
-      )
+              children: [
+                Text(
+                  'No transactions added yet',
+                  style: Theme.of(context).textTheme.title,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/sleeping.png',
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            )
           : ListView.builder(
-        itemCount: userTransactions.length,
-        itemBuilder: (ctx, index) {
-          return Card(
-              elevation: 5,
-              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 30,
-                  child: Padding(
-                      padding: EdgeInsets.all(6),
-                      child: FittedBox(
-                          child: Text(
-                              '\$${userTransactions[index].amount}'))),
-                ),
-                title: Text(
-                  userTransactions[index].title,
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .title,
-                ),
-                subtitle: Text(DateFormat.yMMMMd()
-                    .format(userTransactions[index].date)),
-                trailing: IconButton(
-                  icon: Icon(Icons.delete),
-                  color: Theme
-                      .of(context)
-                      .errorColor,
-                  onPressed: () => deleteTransaction(userTransactions[index].id)
-                  ;,
-                ),
-              ));
-        },
-      ),
+              itemCount: userTransactions.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                    elevation: 5,
+                    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                            padding: EdgeInsets.all(6),
+                            child: FittedBox(
+                                child: Text(
+                                    '\$${userTransactions[index].amount}'))),
+                      ),
+                      title: Text(
+                        userTransactions[index].title,
+                        style: Theme.of(context).textTheme.title,
+                      ),
+                      subtitle: Text(DateFormat.yMMMMd()
+                          .format(userTransactions[index].date)),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        color: Theme.of(context).errorColor,
+                        onPressed: () =>
+                            deleteTransaction(userTransactions[index].id),
+                      ),
+                    ));
+              },
+            ),
     );
   }
 }
