6be7089a3ea8440a3e760700b0bd0d97d14ef6cc
Maxime GELLE <maxime.gelle@gmail.com>
2020-11-07 11:14:27 +0100

    handle bottom keyboard

--- expense_planner/lib/widgets/new_transaction.dart
+++ expense_planner/lib/widgets/new_transaction.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -47,51 +48,53 @@ class _NewTransactionState extends State<NewTransaction> {
       Navigator.of(context).pop(); // Close the modal
     }
 
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextField(
-              controller: this._titleController,
-              onSubmitted: (_) => submitData(),
-              // onChanged: (input) => this._titleInput = input,
-              decoration: InputDecoration(labelText: 'Title'),
-            ),
-            TextField(
-              controller: this._amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => submitData(),
-              // onChanged: (input) => this._amountInput = input,
-              decoration: InputDecoration(labelText: 'Amount'),
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(_seletectedDate == null
-                        ? 'No date choosen'
-                        : 'Picked Date ${DateFormat.yMd().format(_seletectedDate)}'),
-                  ),
-                  FlatButton(
-                      onPressed: _presentDatePicker,
-                      child: Text(
-                        'Choose Date',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      )),
-                ],
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(top: 10, left: 10, right: 10, bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                controller: this._titleController,
+                onSubmitted: (_) => submitData(),
+                // onChanged: (input) => this._titleInput = input,
+                decoration: InputDecoration(labelText: 'Title'),
               ),
-            ),
-            RaisedButton(
-              child: Text('Add Transaction'),
-              color: Theme.of(context).primaryColor,
-              textColor: Theme.of(context).textTheme.button.color,
-              onPressed: submitData,
-            )
-          ],
+              TextField(
+                controller: this._amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => submitData(),
+                // onChanged: (input) => this._amountInput = input,
+                decoration: InputDecoration(labelText: 'Amount'),
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(_seletectedDate == null
+                          ? 'No date choosen'
+                          : 'Picked Date ${DateFormat.yMd().format(_seletectedDate)}'),
+                    ),
+                    FlatButton(
+                        onPressed: _presentDatePicker,
+                        child: Text(
+                          'Choose Date',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )),
+                  ],
+                ),
+              ),
+              RaisedButton(
+                child: Text('Add Transaction'),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).textTheme.button.color,
+                onPressed: submitData,
+              )
+            ],
+          ),
         ),
       ),
     );
