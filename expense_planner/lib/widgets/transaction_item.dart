64bcc18d8abe9e08fca4fd4bbf90036d77247394
Maxime GELLE <maxime.gelle@gmail.com>
2020-11-08 15:49:01 +0100

    Add a key to avoid mixing up widget and elements

--- expense_planner/lib/widgets/transaction_item.dart
+++ expense_planner/lib/widgets/transaction_item.dart
@@ -1,15 +1,38 @@
+import 'dart:math';
+
 import 'package:expense_planner/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
-    this.transaction,
-    this.deleteTransaction,
-  });
+    Key key,
+    @required this.transaction,
+    @required this.deleteTransaction,
+  }) : super(key: key);
 
   final Transaction transaction;
   final Function deleteTransaction;
+
+  @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color _bgColor;
+
+  @override
+  void initState() {
+    const availableColors = [
+      Colors.red,
+      Colors.black,
+      Colors.blue,
+      Colors.purple
+    ];
+
+    _bgColor = availableColors[Random().nextInt(availableColors.length)];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,26 +41,30 @@ class TransactionItem extends StatelessWidget {
         margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
         child: ListTile(
           leading: CircleAvatar(
+            backgroundColor: _bgColor,
             radius: 30,
             child: Padding(
                 padding: const EdgeInsets.all(6),
-                child: FittedBox(child: Text('\$${transaction.amount}'))),
+                child:
+                    FittedBox(child: Text('\$${widget.transaction.amount}'))),
           ),
           title: Text(
-            transaction.title,
+            widget.transaction.title,
             style: Theme.of(context).textTheme.title,
           ),
-          subtitle: Text(DateFormat.yMMMMd().format(transaction.date)),
+          subtitle: Text(DateFormat.yMMMMd().format(widget.transaction.date)),
           trailing: MediaQuery.of(context).size.width > 460
               ? FlatButton.icon(
                   textColor: Theme.of(context).errorColor,
                   label: const Text('Delete'),
                   icon: const Icon(Icons.delete),
-                  onPressed: () => deleteTransaction(transaction.id))
+                  onPressed: () =>
+                      widget.deleteTransaction(widget.transaction.id))
               : IconButton(
                   icon: const Icon(Icons.delete),
                   color: Theme.of(context).errorColor,
-                  onPressed: () => deleteTransaction(transaction.id),
+                  onPressed: () =>
+                      widget.deleteTransaction(widget.transaction.id),
                 ),
         ));
   }
--- expense_planner/lib/widgets/transation_list.dart
+++ expense_planner/lib/widgets/transation_list.dart
@@ -34,13 +34,12 @@ class TransactionList extends StatelessWidget {
               );
             },
           )
-        : ListView.builder(
-            itemCount: userTransactions.length,
-            itemBuilder: (ctx, index) {
-              return TransactionItem(
-                  transaction: userTransactions[index],
-                  deleteTransaction: deleteTransaction);
-            },
-          );
+        : ListView(
+            children: userTransactions
+                .map((tx) => TransactionItem(
+                    key: ValueKey(tx.id),
+                    transaction: tx,
+                    deleteTransaction: deleteTransaction))
+                .toList());
   }
 }
