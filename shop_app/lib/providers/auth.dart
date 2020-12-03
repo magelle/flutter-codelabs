6d12adc02a7d8f7dd3f58abc4b84da8e7a14c01b
Maxime GELLE <maxime.gelle@gmail.com>
2020-12-03 23:46:10 +0100

    error handling

--- shop_app/lib/providers/auth.dart
+++ shop_app/lib/providers/auth.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   static const apiKey = 'AIzaSyCqWQqfjIWP-tsfoBcXFELf6l2Or9dp4vk';
@@ -15,10 +16,17 @@ class Auth with ChangeNotifier {
       String email, String password, String urlSegment) async {
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$apiKey';
-
-    final response = await http.post(url,
-        body: json.encode(
-            {'email': email, 'password': password, 'returnSecureToken': true}));
+    try {
+      final response = await http.post(url,
+          body: json.encode(
+              {'email': email, 'password': password, 'returnSecureToken': true}));
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
